365d1234eb7e6f36a1135ee379ff2f1871bcf8b1
gastongerber1 <gaston.gerber@hotmail.com>
2022-06-03 00:05:46 -0300

    new

--- src/database.js
+++ src/database.js
@@ -1,8 +1,10 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://gastongerber1:Maradona1999.@cluster0.aqvck.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const mongoose = require('mongoose');
+
+const URI = 'mongodb+srv://gastongerber:rosario@cluster0.kxdpj.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(URI)
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));
+
+module.exports = mongoose;
+
